fd12cce6771a97866190e9d5b7946729dda71353
Erick <erickbarretodasilva@gmail.com>
2024-06-09 22:26:51 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Banner />
+</template>
+
+<style scoped></style>
+
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: { Banner }
+}
+</script>
