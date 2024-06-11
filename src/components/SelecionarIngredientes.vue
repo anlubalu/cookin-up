a732ff71509a4fe09c97700d36ca51b28f1f5923
Erick <erickbarretodasilva@gmail.com>
2024-06-11 01:24:41 -0300

    aula de hj

---
+++ src/components/SelecionarIngredientes.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="selecionar-ingredientes">
+        <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+        <p class="paragrafo-lg instrucoes">Selecione abaixo os ingredientes que você quer usar nesta receita:</p>
+        <ul class="categorias">
+            <li v-for"categoria in categorias" :key=""></li>
+        </ul>
+        <p class="paragrafo dica">*Atenção: consideramos que você tem em casa sal, pimenta e água.</p>
+    </section>
+</template>
+
+<script lang="ts">
+export default { data() { return { categorias: [] } } }
+</script>
